e59d9548c050603a8c8893bf78e452ecc23b8793
Faisal <faisalmohd83@users.noreply.github.com>
2019-11-15 11:08:36 +0000

    firebase - initial commit (#80)

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCXsB1B4dO8fOb53UD1NaJY4mkuI8XfjfA",
+    authDomain: "velicham-onlive.firebaseapp.com",
+    databaseURL: "https://velicham-onlive.firebaseio.com",
+    projectId: "velicham-onlive",
+    storageBucket: "",
+    messagingSenderId: "856390731274",
+    appId: "1:856390731274:web:eb83ccba5d0bc871"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
